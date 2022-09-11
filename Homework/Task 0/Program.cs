2bf8e35c3ffd253b78302668cbcbe44550ec9beb
Mikhail <mehan@list.ru>
2022-09-11 18:06:03 +0300

    Done

--- Homework/Task 0/Program.cs
+++ Homework/Task 0/Program.cs
@@ -1,1 +1,42 @@
-﻿
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+const int MIN_ELEMENTS = 100;
+const int MAX_ELEMENTS = 1000;
+
+int[] GenerateArray(int length, int minRandom, int maxRandom)
+{
+    Random rnd = new Random();
+    int[] answer = new int[length];
+    for (int i = 0; i < answer.Length; i++)
+    {
+        answer[i] = rnd.Next(minRandom, maxRandom + 1);
+    }
+    return answer;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + "\t");
+    }
+    System.Console.WriteLine();
+}
+
+int EvenCounter(int[] arr)
+{
+    int counter = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]%2 == 0)
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+Random rnd = new Random();
+int[] array = GenerateArray(rnd.Next(5, 15), MIN_ELEMENTS, MAX_ELEMENTS);
+PrintArray(array);
+System.Console.WriteLine($"В массиве {EvenCounter(array)} четных элементов.");
